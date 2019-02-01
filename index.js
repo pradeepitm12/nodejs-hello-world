2f59b047cea41525e4152e858af35119ef7e6ef1
PRADEEP KUMAR <pradeepitm12@gmail.com>
2019-02-02 01:58:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
 var msg   = process.env.APP_MSG;
+var user  = process.env.USER;
+var pass  = process.env.PASS;
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello "+msg);
+    response.end("Hello "+msg +" User Name = "+user + " password = "+pass);
 
 });
 

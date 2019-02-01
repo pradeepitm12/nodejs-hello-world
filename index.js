fb43fb6e1400021857f0031c69472d5dcc260294
PRADEEP KUMAR <pradeepitm12@gmail.com>
2019-02-02 01:15:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
+var msg   = process.env.APP_MSG;
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end("Hello "+msg);
 
 });
 
